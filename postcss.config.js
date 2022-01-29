2179c5da56a4f2f84c8fda0c8ec322dcc78a4c93
massaaki <eu@massaaki.com>
2022-01-28 23:42:08 -0300

    chore: configure postcss to storybook

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // Add your installed PostCSS plugins here:
+  plugins: [
+    require('autoprefixer'),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-color-rebeccapurple')
+  ]
+}
