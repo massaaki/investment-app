e8dba9d63cd7f209d846f50e285a634f8d7924af
massaaki <eu@massaaki.com>
2022-02-04 23:17:23 -0300

    fix: remove AuthenticationRoute from dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,5 @@
-import AuthenticatedRoute from 'components/utils/AuthenticatedRoute'
 import { DashboardTemplate } from 'templates/Dashboard'
 
 export default function DashboardPage() {
-  return (
-    <AuthenticatedRoute>
-      <DashboardTemplate />
-    </AuthenticatedRoute>
-  )
+  return <DashboardTemplate />
 }
